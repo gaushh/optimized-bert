54a691ec35c490e18e8f3c3f5a6c53500c77131d
gaushh <gsharma14497@gmail.com>
2022-12-04 00:26:45 -0600

    wandb integrated and setup.sh added

--- src/modelling/train_bert.py
+++ src/modelling/train_bert.py
@@ -6,11 +6,14 @@ from transformers import BertTokenizerFast
 from transformers import TrainingArguments
 from transformers import BertConfig, BertForMaskedLM
 from transformers import DataCollatorForLanguageModeling
+import wandb
 
 with open("../config/config.yaml", "r") as yamlfile:
     config = yaml.load(yamlfile, Loader=yaml.FullLoader)
 
-print("Hello 1")
+wandb.init(project="optimized-bert", entity="madridistas")
+
+print("Loading Data ...")
 
 train_path = config['dataset']['processed_train_dir']
 test_path = config['dataset']['processed_test_dir']
@@ -18,7 +21,7 @@ test_path = config['dataset']['processed_test_dir']
 train_dataset = load_from_disk(train_path)
 test_dataset = load_from_disk(test_path)
 
-print("Hello 2")
+print("Reading hyperparameters from config ... ")
 
 vocab_size = config['tokenizer']["vocab_size"]
 max_length = config['tokenizer']["max_length"]
@@ -34,18 +37,16 @@ gradient_accumulation_steps = config['bert']["gradient_accumulation_steps"]
 logging_steps = config['bert']["logging_steps"]
 save_steps = config['bert']["save_steps"]
 
-print("Hello 3")
+print("Loading tokenizer and model ...")
 
 tokenizer = BertTokenizerFast.from_pretrained(tokenizer_path)
 
-print("Hello 4")
 
 model_config = BertConfig(vocab_size=vocab_size, max_position_embeddings=max_length)
 model = BertForMaskedLM(config=model_config)
 
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=mlm_probability)
 
-print("Hello 5")
 
 
 if not os.path.isdir(model_path):
@@ -62,12 +63,11 @@ training_args = TrainingArguments(
     gradient_accumulation_steps=gradient_accumulation_steps,  # accumulating the gradients before updating the weights
     logging_steps=logging_steps,             # evaluate, log and save model checkpoints every 1000 step
     save_steps=save_steps,
+    report_to="wandb"
     # load_best_model_at_end=True,  # whether to load the best model (in terms of loss) at the end of training
     # save_total_limit=3,           # whether you don't have much space so you let only 3 model weights saved in the disk
-)
+    )
 
-
-print("Hello 6")
 
 trainer = Trainer(
     model=model,
@@ -77,8 +77,8 @@ trainer = Trainer(
     eval_dataset=test_dataset,
 )
 
-print("Hello 7")
+print("Starting Training ...")
 
 trainer.train()
 
-print("Hello 8")
+print("Finished")
